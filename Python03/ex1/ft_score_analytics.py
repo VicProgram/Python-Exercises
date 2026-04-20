79695ae59ed121962e02fe1b8df81c0a72ca5142
Victor Abad Romero <159181879+VicProgram@users.noreply.github.com>
2026-04-20 19:15:59 +0200

    Add files via upload

--- Python03/ex1/ft_score_analytics.py
+++ Python03/ex1/ft_score_analytics.py
@@ -1,59 +1,29 @@
-# #! /usr/bin/env python3
-
 import sys
-
-
-# def ft_score_analytics(scorelist: list) -> None:
-#     scores = scorelist
-#     total_players = len(scores)
-#     total_score = sum(scores)
-#     average = total_score / total_players
-#     high_score = max(scores)
-#     low_score = min(scores)
-
-#     print("=== Player Score Analytics ===")
-#     print("Total players: ", total_players)
-#     print(f"Total score: {total_score}")
-#     print(f"Average score:{average:.1f}")
-#     print(f"High score: {high_score}")
-#     print(f"Low score: {low_score}")
-#     print(f"Score range: {high_score - low_score}")
-
-
-# if __name__ == "__main__":
-#     # ft_score_analytics()
-#     scorelist = []
-#     if len(sys.argv) == 1:
-#         print(
-#             "No scores provided. Usage: python3 "
-#             "ft_score_analytics.py < score1> <score2> ..."
-#         )
-#     else:
-#         for arg in sys.argv[1:]:
-#             try:
-#                 arg = int(arg)
-#                 scorelist.append(arg)
-#             except ValueError:
-#                 print("Invalid parameter")
-#     if scorelist:
-#         ft_score_analytics(scorelist)
 
 
 def ft_score_analytics() -> None:
 
+    nsp: str = "No scores provided. Usage: python3 "
+    "ft_score_analytics.py < score1> <score2> ..."
+
     if len(sys.argv) == 1:
-        print(
-            "No scores provided. Usage: python3 "
-            "ft_score_analytics.py < score1> <score2> ..."
-        )
+        print(nsp)
         return
-    try:
+    for arg in sys.argv[1:]:
         score_list = []
-        for arg in sys.argv[1:]:
+        try:
             num = int(arg)
             score_list.append(num)
-    except ValueError:
-        print(f"Invalid parameter '{arg}'")
+
+        except ValueError:
+            print(f"Invalid parameter '{arg}'")
+    if score_list:
+        print_score(score_list)
+    else:
+        print(nsp)
+
+
+def print_score(score_list: list[int]) -> None:
 
     total_players = len(score_list)
     total_score = sum(score_list)
